db6c7cbe1409a4e7915301ec053df16285a792c3
kirilcvetkov92 <kirilcvetkov92@gmail.com>
2019-04-26 19:21:04 +0200

    Fixed indentation level err

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -209,8 +209,8 @@ class TLDetector(object):
 
         return -1, TrafficLight.UNKNOWN
 
-     def get_distance(self, p1, p2):
-            return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2)
+    def get_distance(self, p1, p2):
+        return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2)
 
 if __name__ == '__main__':
     try:
